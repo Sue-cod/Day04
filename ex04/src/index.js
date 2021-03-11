54cf49d48bebfd0ceb5c37b5b6338df70ef2d01e
Sue-cod <snozic892@gmail.com>
2021-03-12 00:44:25 +0100

    Cetvrti zadatak

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main () {
+   
+    // only change code below this line
+    myDecimal = 8.2;
+    myDecimalE = 2.54e-5;
+    // Only change code above this line
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
