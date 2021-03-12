a56d2cb6f32a010bf7bb6c4edbbef142902aa2c1
Sue-cod <snozic892@gmail.com>
2021-03-12 01:00:45 +0100

    Posljednji js zadatak

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+   
+    // only change code below this line
+    var myFirstName = "Cat";
+    var myLastName = "Kitty";
+    var myHomeTown = "Catland";
+    // Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
